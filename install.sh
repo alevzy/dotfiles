56896d227f247d27ec15eb237ec704b1d01f7694
alevzy <alevzy@vault>
2025-06-11 20:25:01 +0000

    Update install.sh for all vim plugins

--- install.sh
+++ install.sh
@@ -6,6 +6,27 @@ ln -sf $PWD/.zshrc $HOME/.zshrc
 
 # get vim plugins
 VIMPACK=$HOME/.vim/pack
-PLUGINDIR=$VIMPACK/default/start/gruvbox
-echo "Cloning gruvbox to $PLUGINDIR"
-git clone --depth=1 git@github.com:morhetz/gruvbox.git $PLUGINDIR
+GITHUB=git@github.com
+PLUGIN_LIST=(
+    "$GITHUB:morhetz/gruvbox.git" "$VIMPACK/default/start/gruvbox" "$VIMPACK/default/start/gruvbox/doc"
+    "$GITHUB:vim-airline/vim-airline.git" "$VIMPACK/dist/start/vim-airline" "$VIMPACK/dist/start/vim-airline/doc"
+    "$GITHUB:vim-airline/vim-airline-themes.git" "$VIMPACK/dist/start/vim-airline-themes" "$VIMPACK/dist/start/vim-airline-themes/doc"
+    "$GITHUB:tpope/vim-fugitive.git" "$VIMPACK/tpope/start/fugitive" "fugitive/doc"
+    "$GITHUB:preservim/nerdtree.git" "$VIMPACK/vendor/start/nerdtree" "$VIMPACK/vendor/start/nerdtree/doc"
+    "$GITHUB:yegappan/lsp.git" "$VIMPACK/downloads/opt/lsp" "$VIMPACK/downloads/opt/lsp/doc"
+)
+COUNT=${#PLUGIN_LIST[@]}
+for (( i=0; i<COUNT; i+=3))
+do
+    SRC=${PLUGIN_LIST[i+0]}
+    DST=${PLUGIN_LIST[i+1]}
+    HLP=${PLUGIN_LIST[i+2]}
+    if [[ ! -d "$DST" ]]; then
+        echo "Installing plugin from $SRC"
+        git clone --depth=1 $SRC $DST
+        vim -u NONE -c "helptags $HLP" -c q
+    else
+        echo "Plugin $SRC is already installed"
+    fi
+done
+
